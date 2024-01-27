93d73fb35d405afe6e87f93f58b8d9b6720e3597
Manuel <manuelgalvez192@gmail.com>
2024-01-27 22:56:22 +0100

    almost completed game

--- assets/GuessingNumberGame/GuessingNumberGame.cpp
+++ assets/GuessingNumberGame/GuessingNumberGame.cpp
@@ -1,20 +1,32 @@
-// GuessingNumberGame.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int userNumber, randomNum, lives = 5, score = 0;
+    while (score != 1000)
+    {
+        std::cout << "The game starts\n";
+        std::cout << "You have 5 lives\n";
+        randomNum = std::rand() % 100;
+        std::cout << "Try to guess the number\n";
+        std::cin >> userNumber;
+        while ((userNumber != randomNum) && (lives > 0))
+        {
+            lives--;
+            std::cout << "Wrong number, try again\n";
+            std::cin >> userNumber;
+        }
+        if (lives == 0)
+        {
+            std::cout << "GAME OVER\n";
+            return (0);
+        }
+        if (userNumber == randomNum)
+        {
+            std::cout << "Score\n" << score + 100;
+            std::cout << "You win the round\n"
+        }
+    }
+    std::cout << "YOU WIN THE GAME";
+    return (0);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
