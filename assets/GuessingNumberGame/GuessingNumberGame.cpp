ee96b67d85a69706ddeab3d4822fe9ee40edc0ac
Manuel <manuelgalvez192@gmail.com>
2024-01-28 01:01:50 +0100

    some fixes to the proyect

--- assets/GuessingNumberGame/GuessingNumberGame.cpp
+++ assets/GuessingNumberGame/GuessingNumberGame.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
-#include <random>
+#include <stdlib.h>
 
 int main()
 {
     int userNumber, randomNum, lives = 5, score = 0;
+    std::cout << "The game starts\n";
     while (score != 1000)
     {
-        std::cout << "You have 5 lives\n";
-        randomNum = std::rand() % 10;
+        std::cout << "You have " << lives << " lives\n";
+        srand(time(NULL));
+        randomNum = rand() % 101;
         std::cout << "Try to guess the number to win $1,000\n";
         std::cin >> userNumber;
         while ((userNumber != randomNum) && (lives > 0))
         {
             lives--;
-            std::cout << "Wrong number, try again. You have " << lives << " left\n";
+            std::cout << "Wrong number, try again. You have " << lives << " lives remaining\n";
             if (userNumber < randomNum)
             {
                 std::cout << "The magic number is greater than the number you are trying to guess\n";
